3a6935362fd2e59ddbe457f28dbac98637811d9b
Frank Holler <4014957+fholler0371@users.noreply.github.com>
2020-06-18 23:36:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,40 +5,31 @@ basepath="$dir/smarthome";
 
 if [ -d "$basepath" ];then
 
+cd $basepath/tmp
+git clone https://github.com/fholler0371/smarthome.git
 echo "update"
 
 else
+
 sudo apt-get update
 sudo apt-get install python3 python3-pip git -y
-
 sudo pip3 install virtualenv
-
-
 sudo mkdir $basepath 
-
 sudo chown $(id -u):$(id -g) $basepath
-
 cd $dir
-
-git clone --branch v0.0-beta https://github.com/fholler0371/smarthome.git
-
+git clone https://github.com/fholler0371/smarthome.git
 cd $basepath
-
 virtualenv env
 
 fi
 
+cd $basepath
+env/bin/pip install -r requirements.txt
 file="$basepath/smarthome.py"
-
-echo $file
-
 sed -i -e "1d" $file
-
 sed  -i "1i #!$basepath/env/bin/python" $file
-
 sudo chown -R $(id -u):$(id -g) $basepath
 sudo chmod +x $file
-
 $file install
 $file restart
 
