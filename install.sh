c51c1d8fea8fdcf2f3fa92743878d92750d4e568
Frank Holler <4014957+fholler0371@users.noreply.github.com>
2020-06-18 23:51:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ basepath="$dir/smarthome";
 if [ -d "$basepath" ];then
 
 cd $basepath/tmp
+rm -R --force smarthome
 git clone https://github.com/fholler0371/smarthome.git
 cd $basepath
-ls -l
 cp -R tmp/smarthome/* ./
 rm -R --force tmp/smarthome
 
