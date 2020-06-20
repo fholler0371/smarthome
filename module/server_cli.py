34db12bc7bfa744e52a4fa91bbe94296580102b6
fholler0371 <4014957+fholler0371@users.noreply.github.com>
2020-06-20 20:19:45 +0200

    config

--- module/server_cli.py
+++ module/server_cli.py
@@ -1,6 +1,9 @@
 from module import modul_base as base
 import json
 import subprocess
+import os, time
+import netifaces
+import psutil
 
 class modul(base):
     def __init__(cls, sh, cfg):
@@ -23,7 +26,10 @@ class modul(base):
         if len(menu_levels) == 1:
             out = json.dumps({'label': 'Bitte Modul aussuchen', 'entries' : [
                 {'label' : 'Sytemupdate', 'cmd': 'CMD ' + str(menu_levels[0]) + '.1'},
-                {'label' : 'Reboot', 'cmd': 'CMD ' + str(menu_levels[0]) + '.2'}
+                {'label' : 'Reboot', 'cmd': 'CMD ' + str(menu_levels[0]) + '.2'},
+                {'label' : 'Smarthome neustarten', 'cmd': 'CMD ' + str(menu_levels[0]) + '.3'},
+                {'label' : 'Smarthome upgrade', 'cmd': 'CMD ' + str(menu_levels[0]) + '.4'},
+                {'label' : 'Status', 'cmd': 'CMD ' + str(menu_levels[0]) + '.5'}
                 ], 'exit':None})
         return out
 
@@ -43,4 +49,27 @@ class modul(base):
             elif cmd_levels[1] == '2':
                 response1 = subprocess.Popen(('sudo reboot').split(' '), stdout=subprocess.PIPE).stdout.read()
                 out = response1.decode(errors= 'backslashreplace')
+            elif cmd_levels[1] == '3':
+                cls.sh.running = False
+                out = 'Neustart aktiviert'
+            elif cmd_levels[1] == '4':
+                name = cls.sh.basepath + '/tmp/install_it.sh'
+                f = open(name, 'w')
+                f.write('wget -q -O - https://raw.githubusercontent.com/fholler0371/smarthome/master/install.sh | bash')
+                f.close()
+                response1 = subprocess.Popen(('bash ' + name).split(' '), stdout=subprocess.PIPE).stdout.read()
+                out = response1.decode(errors= 'backslashreplace')
+                os.remove(name)
+            elif cmd_levels[1] == '5':
+                response = subprocess.Popen(('hostname -f').split(' '), stdout=subprocess.PIPE).stdout.read()
+                out = 'Hostname: ' + response.decode(errors= 'backslashreplace')
+                net = []
+                for interface in netifaces.interfaces():
+                    if 'broadcast' in netifaces.ifaddresses(interface)[netifaces.AF_INET][0]:
+                        net.append(interface)
+                for interface in net:
+                    out += 'Netzwerk ' + str(interface) + ': ' + netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr'] + '\n'
+                out += 'Laufzeit: ' + str(int(time.time() - psutil.boot_time())) + '\n'
+                response = subprocess.Popen(('sudo systemctl status ' + cls.sh.basename + '.service').split(' '), stdout=subprocess.PIPE).stdout.read()
+                out += response.decode(errors= 'backslashreplace')
         return out
