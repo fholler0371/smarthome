041f4a31945eca212e7ebfa994ad825e65789450
fholler0371 <4014957+fholler0371@users.noreply.github.com>
2020-06-22 18:08:10 +0200

    2020-06-22

--- module/backup.py
+++ module/backup.py
@@ -4,7 +4,6 @@ class modul(base):
     def __init__(cls, sh, cfg):
         base.__init__(cls, sh, cfg)
         cls.sh.log.info('__init__')
-        cls.has_menu = True
         cls.name = "Backup"
 
     def start(cls):
@@ -19,13 +18,3 @@ class modul(base):
     def backup(cls):
         cls.sh.log.info('backup')
 
-    def menu_cli(cls, menu):
-        cls.sh.log.info('menu_cli')
-        cls.sh.log.debug(menu)
-        out = '-'
-        menu_levels = menu[5:].split('.')
-        if len(menu_levels) == 1:
-            out = json.dumps({'label': 'Bitte Aktion aussuchen', 'entries' : [
-                {'label' : 'Status', 'cmd': 'CMD ' + str(menu_levels[0]) + '.1'},
-                {'label' : 'Cronzeit einstellen', 'cmd': 'MENU ' + str(menu_levels[0]) + '.1'}
-                ], 'exit':None})
