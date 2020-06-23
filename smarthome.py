d6005e0e794827b466b4b2082dee21aad6cc1a2c
Frank Holler <4014957+fholler0371@users.noreply.github.com>
2020-06-23 10:49:52 +0200

    Update smarthome.py

--- smarthome.py
+++ smarthome.py
@@ -1,7 +1,36 @@
 #!/media/pi/data/SM/smarthome/env/bin/python3
 
+# -*- coding: utf-8 -*-
+"""SmartHome Start Script
+
+Dieses Script ist zum Starten des SmartHome Programms
+
+Es sind folgende Parameter möglich:
+    install
+        Installiert das Script als Dienst
+    uninstall
+        Deinstalliert den Dient
+    start
+        Starte den Dienst
+    stop
+        Stopt den Dient
+    restart
+        Führt nach einander Start stop und start aus
+    status
+        gibt Informationen über den Dienst aus
+   
+Der Pfad zum Interpreter in der ersten Zeile wir durch das Installscript
+auf den passenden Interpreter im virtuellen Enviroment angepasst.
+
+Todo:
+
+Version:
+"""
+
 from bin.smarthome import smarthome as sh
 
 smarthome = sh()
-smarthome.run()
+''' Lädt das Hauptmodul vom Smarthome '''
 
+smarthome.run()
+''' Startet die das Modul '''
