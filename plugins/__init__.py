448af79e2c6aed963585bb9731c80a7ff6eb9de0
fholler0371 <4014957+fholler0371@users.noreply.github.com>
2020-06-23 19:25:14 +0200

    2020-06-23

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,3 +1,7 @@
+import os
+
+import importlib.util
+
 class master():
     def __init__(self, sh):
         sh.log.info('init master')
@@ -6,9 +10,29 @@ class master():
         self.plugins = []
 
     def load(self, in_data):
-        sh.log.info('load master')
-        print(typeof(in_data))
+        self.sh.log.info('load master')
+        if isinstance(in_data, str):
+            data = [in_data]
+        for name in data:
+            self._load(name)
+
+    def _load(self, name):
+        self.sh.log.info('load: ' + name)
+        file = self.sh.const.path + '/plugins/' + name + '/__init__.py'
+        if not os.path.exists(file):
+            self.sh.log.error('plugin nicht gefunden: ' + name)
+            return None
+        spec = importlib.util.spec_from_file_location(name, file)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        if not hasattr(mod, 'plugin'):
+            self.sh.log.error('Klasse plugin nicht gefunden')
+            return None
+        cls = mod.plugin(self.sh, name)
+
 
 class base():
-    def __init__(self):
+    def __init__(self, sh, name):
+        self.sh = sh
+        self.name = name
         self.requirements = []
---
+++ plugins/webserver_backend_master/__init__.py
@@ -0,0 +1,6 @@
+import plugins
+
+class plugin(plugins.base):
+    def __init__(self, sh, name):
+         plugins.base.__init__(self, sh, name)
+         pass
