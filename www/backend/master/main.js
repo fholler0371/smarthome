d313fbeae76f5a45e3d98a3ca00866866acca999
fholler0371 <4014957+fholler0371@users.noreply.github.com>
2020-06-24 21:31:05 +0200

    2020-06-24

--- www/backend/master/main.js
+++ www/backend/master/main.js
@@ -1,13 +1,25 @@
 requirejs.config({
     baseUrl: '/lib',
     paths: {
-        jquery: 'jquery/jquery-3.5.1.min'
+        jquery: 'jquery/jquery-3.5.1.min',
+        jqxcore: 'jqwidgets/jqxcore',
+        jqxsplitter: 'jqwidgets/jqxsplitter'
+    },
+    shim:{
+        jqxcore : {
+            deps : ['jquery']
+          },
+        jqxsplitter : {
+            deps : ['jqxcore']
+          }
     }
 });
 
 requirejs(['jquery'], function( $ ) {
   $(document).ready(function() {
     $('body').append('<div id="mainSplitter"><div id="panel-master">Panel 1</div><div id="panel-clients">Panel 2</div></div>')
-    console.log( $ )
+    requirejs(['jqxsplitter'], function(
+      console.log( $ )
+    });
   });
 });
