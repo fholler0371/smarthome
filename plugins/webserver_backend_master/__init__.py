3a37d644cb3f2ea954bcdcee24a6ab1e4c21acd9
fholler0371 <4014957+fholler0371@users.noreply.github.com>
2020-06-26 22:43:50 +0200

    liste mit Servern erstellt die zu pingen sind

--- plugins/webserver_backend_master/__init__.py
+++ plugins/webserver_backend_master/__init__.py
@@ -19,6 +19,21 @@ Verlauf:
 """
 
 import plugins
+from threading import Thread
+
+import bin.ping as ping
+
+class scanThread(Thread):
+    def __init__(self, log, net, pl):
+        Thread.__init__(self)
+        self.net = net
+        self.pl = pl
+        self.log = log
+
+    def run(self):
+        ping_hosts = ping.scan(self.log, self.net)
+        for host in ping_hosts:
+            print(host)
 
 class plugin(plugins.base):
     ''' Klasse des Plugins mit den Standard Parametern '''
@@ -46,6 +61,7 @@ class plugin(plugins.base):
 
         ''' setzen der defaults '''
         self.server = None
+        self.scanning = False
 
     def _create_config(self):
         self.sh.log.info('_create_config')
@@ -81,5 +97,22 @@ class plugin(plugins.base):
         if self.server:
             self.lib['webserver'].webserver_stop(self.server, self.cfg['port'])
 
+    def _scan_hosts(self):
+        if not('network' in self.cfg):
+            net = ping.guess_network()
+            if net != '':
+                self.cfg['network'] = net
+                self.sh.cfg.data['plugins'][self.name]['network'] = net
+                self.sh.cfg.save()
+        if 'network' in self.cfg:
+            th = scanThread(self.sh.log, self.cfg['network'], self)
+            th.start()
+            print('start scanning')
+            print(self.cfg)
+            self.scanning = True
+
     def api(self, data_in):
+        data = data_in['data']
+        if data['cmd'] == 'scan_clients':
+            self._scan_hosts()
         return data_in['data']
